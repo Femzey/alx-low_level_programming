376116f11a2ef86ebcd8a1bff87347a402ab676e
femzey <femiihinmikalu01@gmail.com>
2022-11-03 09:01:42 -0700

    ok

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -3,7 +3,7 @@
 /**
  *_print_rev_recursion - function that prints a string in reverse.
  *
- *s: The pointer variable to string s
+ *@s: The pointer variable to string s
  */
 
 void _print_rev_recursion(char *s)
